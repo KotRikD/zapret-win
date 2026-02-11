2a53bcc322a4096b43ebabd2b94f8e73022f7fb2
Mikhail Babynichev <i@kotrik.ru>
2026-02-11 12:47:17 +0300

    feat: update ipset

--- zapret-bundle/ВОТ ЭТО ЗАПУСКАТЬ.cmd
+++ zapret-bundle/ВОТ ЭТО ЗАПУСКАТЬ.cmd
@@ -9,7 +9,7 @@ call "%BASE%files\ts_timestamps_enabler.cmd"
 
 :: Ставим стандартный набор стратегий
 set ZAPRET_ARGS_UDP=--dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=10 --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp="%BASE%files\quic_initial.bin" --dpi-desync-cutoff=n2
-set ZAPRET_ARGS_QUIC=--dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%BASE%files\quic_initial.bin"
+set ZAPRET_ARGS_QUIC=--dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=11 --dpi-desync-fake-quic="%BASE%files\quic_initial.bin"  --dpi-desync-cutoff=n2
 
 :: Здесь вообщем набор файлов, нужный для автолиста
 set IPSET=--ipset="%BASE%files\ipset.list" --ipset-ip=0.0.0.0
@@ -24,7 +24,7 @@ start "zapret: http,https,quic" "%BASE%\bin\winws.exe" ^
 --wf-tcp=80,443,5222,2000-3000 --wf-udp=443,993-9998,10000-27014,50000-65535 ^
 --comment "LoL (EUNE)"             --filter-tcp=2099 %IPSET% --dpi-desync=syndata --new ^
 --comment "HTTPS/2"                --filter-tcp=443,5222,2000-3000 %IPSET% %ZAPRET_ARGS% %ZAPRET_HOSTLISTS% --new ^
---comment "UDP"                    --filter-udp=993-9998,10000-27014,50000-65535 %IPSET% %ZAPRET_ARGS_UDP% --new ^
+--comment "UDP"                    --filter-udp=598-600,993-9998,10000-28000,50000-65535 %IPSET% %ZAPRET_ARGS_UDP% --new ^
 --comment "TLS/QUIC"               --filter-udp=443 %IPSET% %ZAPRET_ARGS_QUIC%
 
  
